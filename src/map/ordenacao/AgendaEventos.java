a3307e9915d974c4a10a19f6cbd2fa8e3d2a6bfa
ailton78 <sd.ailtonsampaio@gmail.com>
2024-08-14 19:51:10 -0300

    Concluido

--- src/map/ordenacao/AgendaEventos.java
+++ src/map/ordenacao/AgendaEventos.java
@@ -2,9 +2,7 @@ package map.ordenacao;
 
 import java.time.LocalDate;
 import java.time.Month;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class AgendaEventos {
 
@@ -40,19 +38,9 @@ public class AgendaEventos {
         }
     }
     public static void main(String[] args) {
-        AgendaEventos agendaEventos = new AgendaEventos();
-
-        // Adiciona eventos à agenda
-        agendaEventos.adicionarEvento(LocalDate.of(2022, Month.JULY, 15), "Conferência de Tecnologia", "Palestrante renomado");
-        agendaEventos.adicionarEvento(LocalDate.of(2022, 7, 9), "Workshop de Programação", "Aula prática de desenvolvimento");
-        agendaEventos.adicionarEvento(LocalDate.of(2000, 1, 10), "Lançamento de Software", "Demonstração da nova versão");
-        agendaEventos.adicionarEvento(LocalDate.of(2023, Month.AUGUST, 28), "Hackathon de Inovação", "Competição de soluções criativas");
-        agendaEventos.adicionarEvento(LocalDate.of(2024, 9, 20), "Seminário de Inteligência Artificial", "Discussão sobre IA avançada");
-
-        // Exibe a agenda completa de eventos
-        agendaEventos.exibirAgenda();
-
-        // Obtém e exibe o próximo evento na agenda
-        agendaEventos.obterProximoEvento();
+        List<Character> meu = Arrays.asList('a', 'b');
+        System.out.println(meu);
+        Set<String> coresDaBandeiraDoBrasil = new TreeSet<>(Arrays.asList("verde", "amarelo", "azul", "branco")) ;
+        System.out.println(coresDaBandeiraDoBrasil);
     }
 }
